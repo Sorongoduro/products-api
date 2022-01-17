d25e338c24f441661f4b2b28f46a57518da58dc0
Sorongoduro <lucascapalbo@hotmail.com.ar>
2022-01-17 12:13:32 -0300

    deleted file

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 var express = require('express');
 const app = express();
 const routerProducts = require('./routes/productos')
-
 require('./db/mongoose')
 let cors = require('cors')
 const router = express.Router()
-
 const port = process.env.PORT || 3001
 
 
 
 app.use(cors())
 app.use(express.json());
-
-
 app.use(router)
 app.use(routerProducts)
-app.use(express.urlencoded({ extended: false }));
 
 
 app.listen(port, () => {
